61b9fd4766348a363295ddccf7547550da048051
David Sugar <tychosoft@gmail.com>
2018-12-21 08:49:48 -0500

    further api auth work

--- webkit/apiauth.rb
+++ webkit/apiauth.rb
@@ -14,21 +14,25 @@ class ApiAuth < Sinatra::Base
     return env[API_AUTHORIZATION] != nil
   end
 
+  def authorized
+    return env[API_AUTHORIZATION]
+  end
+
   before do
     return if !authorized?      # no auth header, so skip
     if env[API_AUTHORIZATON] && env[API_AUTHORIZATION].split(':').length == 2
       auth = env['HTTP_AUTHORIZATION'].split(':')
-  else
-    halt 401
-  end
+    else
+      halt 401
+    end
     data = request.path
     data = "#{data}?#{request.query_string}" if request.query_string.present?
     if ['POST', 'PUT', 'PATCH'].include? request.request_method
       request.body.rewind
       data += request.body.read
     end
-    # TODO:
-    # We will verify auth id against request signed by the endpoint web token
-    
+    # TODO: verify auth id against request signed by the endpoint web token,
+    # on success reset env with effective authorization actually used
+    env[API_AUTHORIZATION] = auth[0]
   end
 end
